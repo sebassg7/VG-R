28ed41b8d05ddd4a9e94788b5bdd589414752f37
sebassg7 <sebastlopez@unicauca.edu.co>
2023-04-13 09:51:40 -0500

    Clase 4

--- game.js
+++ game.js
@@ -1,32 +1,34 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 
-window.addEventListener('load', startGame);
+let canvasSize;
+let elementsSize;
+
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
+
+function setCanvasSize() {
+  if (window.innerHeight > window.innerWidth) {
+    canvasSize = window.innerWidth * 0.8;
+  } else {
+    canvasSize = window.innerHeight * 0.8;
+  }
+  
+  canvas.setAttribute('width', canvasSize);
+  canvas.setAttribute('height', canvasSize);
+  
+  elementsSize = canvasSize / 10;
+
+  startGame();
+}
 
 function startGame() {
+  console.log({ canvasSize, elementsSize });
 
-    let canvasSize;
+  game.font = elementsSize + 'px Verdana';
+  game.textAlign = 'end';
 
-    // canvasSize = window.innerHeight * 0.75;
-   
-    if (window.innerHeight > window.innerWidth) {
-      canvasSize = window.innerWidth*0.8;
-    } else {
-      canvasSize = window.innerHeight*0.7;
-    }
-    
-    canvas.setAttribute('width', canvasSize);
-    canvas.setAttribute('height', canvasSize);
-    
-console.log(canvasSize);
-
-  // game.fillRect(0,50,100,100);
-  // game.clearRect(50,50,50,50);
-  // game.clearRect()
-  // game.clearRect(0,0,50,50);
-
-  // game.font = '25px Verdana'
-  // game.fillStyle = 'purple';
-  // game.textAlign = 'center';
-  // game.fillText('Platzi', 25, 25);
-};
+  for (let i = 1; i <= 10; i++) {
+    game.fillText(emojis['X'], elementsSize, elementsSize * i);
+  }
+}
